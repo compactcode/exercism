739890b693ad2d18ddfd06879c073cdaf53549cb
Shanon McQuay <shanonmcquay@gmail.com>
2017-07-01 19:53:34 +1000

    Replace for loop with map&join.

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn verse(number: u32) -> String {
+pub fn verse(number: u8) -> String {
     match number {
         0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
         1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
@@ -7,12 +7,6 @@ pub fn verse(number: u32) -> String {
     }
 }
 
-pub fn sing(finish: u32, start: u32) -> String {
-    let mut result: Vec<String> = Vec::new();
-
-    for i in (start..finish + 1).rev() {
-        result.push(verse(i));
-    }
-
-    result.join("\n")
+pub fn sing(finish: u8, start: u8) -> String {
+    (start..finish + 1).rev().map(verse).collect::<Vec<String>>().join("\n")
 }
