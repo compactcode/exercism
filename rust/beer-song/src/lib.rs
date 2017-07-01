67590b11356374313818159da8e7f47ccd4681f9
Shanon McQuay <shanonmcquay@gmail.com>
2017-07-01 19:59:53 +1000

    Combine the last two verse conditions with a formatter.

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,9 +1,15 @@
+fn bottles(bottles: u8) -> String {
+    match bottles {
+        1 => String::from("1 bottle"),
+        _ => format!("{} bottles", bottles),
+    }
+}
+
 pub fn verse(number: u8) -> String {
     match number {
         0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
         1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
-        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
-        _ => String::from(format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.\n", number, number - 1)),
+        _ => format!("{0} of beer on the wall, {0} of beer.\nTake one down and pass it around, {1} of beer on the wall.\n", bottles(number), bottles(number - 1)),
     }
 }
 
