9666a1bb7d9cfe8c0b92045b7da178dedb399b75
Shanon McQuay <shanonmcquay@gmail.com>
2017-07-02 14:26:23 +1000

    Replace the encode loop with a while.

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -2,24 +2,19 @@ pub fn encode(content: &str) -> String {
     let mut result = String::new();
     let mut chars = content.chars().peekable();
     let mut curr_count = 0;
-    loop {
-        match chars.next() {
-            Some(curr) => {
-                curr_count += 1;
-                match chars.peek() {
-                    Some(n) if n == &curr => {
-                        // Do nothing.
-                    }
-                    _ => {
-                        match curr_count {
-                            1 => result.push(curr),
-                            _ => result.push_str(&format!("{}{}", curr_count, curr))
-                        }
-                        curr_count = 0;
-                    }
+    while let Some(curr) = chars.next() {
+        curr_count += 1;
+        match chars.peek() {
+            Some(next) if next == &curr => {
+                // Do nothing.
+            }
+            _ => {
+                match curr_count {
+                    1 => result.push(curr),
+                    _ => result.push_str(&format!("{}{}", curr_count, curr))
                 }
-            },
-            None => break
+                curr_count = 0;
+            }
         }
     }
     result
