f0900a9d220740728103ae8b7ee7321ce6c080ca
Shanon McQuay <shanonmcquay@gmail.com>
2017-07-13 11:33:32 +1000

    Cleanup the count method a bit.

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,15 +1,14 @@
 use std::collections::HashMap;
 
+const NUCLEOTIDES: &str = "ACGT";
+
 fn is_nucleotide(c: char) -> bool {
-    match c {
-        'A' | 'C' | 'G' | 'T' => true,
-        _ => false
-    }
+    NUCLEOTIDES.contains(c)
 }
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, ()> {
     if is_nucleotide(nucleotide) && dna.chars().all(is_nucleotide) {
-        Ok(dna.chars().filter(|&x| x == nucleotide).count())
+        Ok(dna.matches(nucleotide).count())
     } else {
         Err(())
     }
