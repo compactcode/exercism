39bd843087df5ce03aba752db7d5e0b8860fd3db
Shanon McQuay <shanonmcquay@gmail.com>
2017-07-13 11:53:20 +1000

    Clean up nucleotide_counts.

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -15,16 +15,11 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, ()> {
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, ()> {
-    if dna.chars().all(is_nucleotide) {
-        let mut result = HashMap::new();
+    let mut result = HashMap::new();
 
-        for nucleotide in NUCLEOTIDES.chars() {
-            result.insert(nucleotide, count(nucleotide, dna).unwrap());
-        }
-
-        Ok(result)
-    } else {
-        Err(())
+    for nucleotide in NUCLEOTIDES.chars() {
+        result.insert(nucleotide, try!(count(nucleotide, dna)));
     }
 
+    Ok(result)
 }
