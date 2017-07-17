d44af26ab5bea43af94d78c1d2e88fde1c15f4c2
Shanon McQuay <shanonmcquay@gmail.com>
2017-07-17 10:08:04 +1000

    Avoid the inverted logic.

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,13 +1,9 @@
 use std::collections::HashMap;
 
-fn invalid_char(c: char) -> bool {
-    !c.is_alphanumeric() && !c.is_whitespace()
-}
-
 pub fn word_count(sentence: &str) -> HashMap<String, u32> {
     sentence
         .to_lowercase()
-        .replace(invalid_char, "")
+        .chars().filter(|c| c.is_alphanumeric() || c.is_whitespace()).collect::<String>()
         .split_whitespace()
         .fold(HashMap::new(), |mut words, word| {
             *words.entry(word.to_string()).or_insert(0) += 1;
