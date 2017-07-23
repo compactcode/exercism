759f3a01e83472c5f6a1a2578cfcb22fcd9721af
Shanon McQuay <shanonmcquay@gmail.com>
2017-07-23 21:42:09 +1000

    Use chunks.

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -2,35 +2,29 @@
 use std::ascii::AsciiExt;
 
 pub fn encode(input: &str) -> String {
-    substitue(input)
+    decode(input)
+        .chars()
+        .collect::<Vec<_>>()
+        .chunks(5)
+        .map(|w| w.iter().collect::<String>())
+        .collect::<Vec<_>>()
+        .join(" ")
 }
 
 pub fn decode(input: &str) -> String {
-    substitue(input).replace(" ", "")
-}
-
-fn substitue(input: &str) -> String {
     input
         .chars()
         .into_iter()
         .filter(char::is_ascii_alphanumeric)
         .flat_map(char::to_lowercase)
-        .map(translate)
-        .fold(String::new(), |mut encoded, c| {
-            if encoded.len() % 6 == 0 {
-                encoded.push(' ');
-            }
-            encoded.push(c);
-            encoded
-        })
-        .trim()
-        .to_string()
+        .map(swap_char)
+        .collect()
 }
 
 const UPPER: u8 = 'z' as u8;
 const LOWER: u8 = 'a' as u8;
 
-fn translate(c: char) -> char {
+fn swap_char(c: char) -> char {
     if c.is_digit(10) {
         c
     } else {
