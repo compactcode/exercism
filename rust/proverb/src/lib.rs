dc9bddefe70fb238ebfbbbfd535f4b2138097aa2
Shanon McQuay <shanonmcquay@gmail.com>
2017-12-29 16:14:35 +1100

    Remove the mutation.

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,13 +1,22 @@
+use std::iter::once;
+
 pub fn build_proverb(list: Vec<&str>) -> String {
-   let mut results: Vec<String> = list
-       .windows(2)
-       .map(|pair| format!("For want of a {} the {} was lost.", pair[0], pair[1]))
-       .collect();
-
    match list.first() {
-       Some(first) => results.push(format!("And all for the want of a {}.", first)),
-       None        => ()
-   }
+       Some(first) => list
+           .windows(2)
+           .map(format_pair)
+           .chain(once(format_end(first)))
+           .collect::<Vec<String>>()
+           .join("\n"),
 
-   return results.join("\n");
+       None => String::new()
+   }
+}
+
+fn format_pair(pair: &[&str]) -> String {
+   format!("For want of a {} the {} was lost.", pair[0], pair[1])
+}
+
+fn format_end(last: &str) -> String {
+   format!("And all for the want of a {}.", last)
 }
