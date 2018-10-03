08c9dac162f7fdd17a3d0fb4261e83dd149f2c1e
Shanon McQuay <shanonmcquay@gmail.com>
2018-10-03 18:47:19 +1000

    Simplify the filtering logic.

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -3,9 +3,9 @@ use std::collections::HashMap;
 pub fn check(candidate: &str) -> bool {
     let mut char_counts: HashMap<char, usize> = HashMap::new();
 
-    for x in candidate.to_lowercase().chars().filter(|x| *x != '-').filter(|x| !x.is_whitespace()) {
+    for x in candidate.to_lowercase().chars().filter(|x| x.is_alphabetic()) {
         char_counts.entry(x).and_modify(|x| { *x += 1 }).or_insert(1);
     }
 
-    return !char_counts.values().any(|x| *x > 1);
+    return char_counts.values().all(|x| *x == 1);
 }
