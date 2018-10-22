478a7d31294fa11a34406ef4e5488d4a62eb490e
Shanon McQuay <shanonmcquay@gmail.com>
2018-10-22 12:04:46 +1100

    An accumulate implementation.

---
+++ rust/accumulate/src/lib.rs
@@ -0,0 +1,8 @@
+/// What should the type of _function be?
+pub fn map<I, O, F>(input: Vec<I>, mut function: F) -> Vec<O> where F: FnMut(I) -> O {
+    let mut output = Vec::with_capacity(input.len());
+    for x in input {
+        output.push(function(x));
+    }
+    return output;
+}
